982082c54a7c3224e93ddd47ea7400b4efd7fd5d
03bennej <55380591+03bennej@users.noreply.github.com>
2022-08-18 15:00:07 -0400

    Update gbgw.py

--- gbgw.py
+++ gbgw.py
@@ -6,6 +6,7 @@ Reference: A fast algorithm for convolution integrals with space and time varian
 Erez Gilad, Jost von Hardenberg (2006)
 
 @author: Jamie Bennett
+
 """
 
 import cupy as cp
@@ -21,7 +22,6 @@ def gfunc(b, w, Hf, phi, phil, nx, ny, Nl):
     wf = cp.fft.fft2(w)
     z1 = cp.zeros((nx, ny)).astype('float32')
     z2 = cp.zeros((nx, ny)).astype('float32')
-    ab = cp.zeros((nx, ny)).astype('float32')
     for l in range(0, Nl):
         alphal = 2 * phi**2 / (phi**2+(phil**2)[l])
         for j in range(0, Nl):
@@ -30,8 +30,6 @@ def gfunc(b, w, Hf, phi, phil, nx, ny, Nl):
             else:
                 alphal *= (phi**2 - phil[j]**2) * (phil[l]**2 + phil[j]**2) \
                 / ((phil[l]**2 - phil[j]**2)*(phi**2 + phil[j]**2))  
-        ab = cp.fft.fft2(alphal * b)
-        cf = cp.real(cp.fft.ifft2(wf * Hf[l,:,:]))
-        z1 += alphal * cf[:, :]
-        z2 += cp.real(cp.fft.ifft2(ab * Hf[l,:,:]))
+        z1 += alphal * cp.real(cp.fft.ifft2(wf * Hf[l,:,:]))
+        z2 += cp.real(cp.fft.ifft2(cp.fft.fft2(alphal * b) * Hf[l,:,:]))
     return z1, z2
